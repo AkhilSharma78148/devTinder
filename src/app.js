2902b26b37a87146f95d7413c4c9902b7406af11
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-05-05 17:31:55 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,32 @@ console.log("Dev tinder app");
 
 const express = require("express");
 
-const app = express(); //creating the server
+const app = express(); //creating the express server
 
-app.use("/", (req, res) => {
-    res.send("Namaste Akhil!");
-});
+// app.use("/", (req, res) => {
+//     res.send("Namaste Akhil!");
+// });
 
 app.use("/hello", (req, res) => {
     res.send("Hello from the server");
+});
+
+app.get("/abc", (req, res) => {
+    res.send({"firstname": "Akhil", "lastname": "Sharma"});
+});
+
+// app.get("/ab?c", (req, res) => {
+//     res.send({"firstname": "Akhil", "lastname": "Sharma"});
+// });
+
+app.get("/user", (req, res) => {
+    console.log(req.query);
+    res.send({"firstname": "Vijay", "lastname": "Kumar"});
+});
+
+app.get("/data/:userId/:name/:password", (req, res) => {
+    console.log(req.query);
+    res.send({"firstname": "Vijay", "lastname": "Kumar"});
 });
 
 app.listen(7777, () => {
