56b0fa0d1f037a898a35e8718d0f129683428b1d
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-05-05 18:32:53 +0530

    add the middlewares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 console.log("Dev tinder app");
 
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express(); //creating the express server
 
@@ -8,17 +9,17 @@ const app = express(); //creating the express server
 //     res.send("Namaste Akhil!");
 // });
 
-app.use(
-    "/user", 
-    (req, res, next) => {
-        //route handler
-        // res.send("Route handler1");
-        next();
-    },
-    (req, res) => {
-        res.send("Route handler2");
-    }
-);
+// app.use(
+//     "/user", 
+//     (req, res, next) => {
+//         //route handler
+//         // res.send("Route handler1");
+//         next();
+//     },
+//     (req, res) => {
+//         res.send("Route handler2");
+//     }
+// );
 
 // app.use("/hello", (req, res) => {
 //     res.send("Hello from the server");
@@ -41,6 +42,27 @@ app.use(
 //     console.log(req.query);
 //     res.send({"firstname": "Vijay", "lastname": "Kumar"});
 // });
+
+// Handle Auth Middleware for all GET, POST, DELETE
+app.use("/admin", adminAuth);
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("Admin get all data");
+});
+
+app.get("/user/login", (req, res) => {
+    console.log(req.query);
+    res.send("Login User");
+});
+
+app.get("/user/data", userAuth , (req, res) => {
+    console.log(req.query);
+    res.send({"firstname": "Vijay", "lastname": "Kumar"});
+});
+
+app.get("/admin/deleteData", (req, res) => {
+    res.send("Admin delete the data");
+});
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777...");
