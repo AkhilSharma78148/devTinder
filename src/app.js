999e65207bda73cabb25306941bdd11beb35fdf7
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-05-06 16:13:43 +0530

    jwt and cookies singup and profile api

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ const app = express(); //creating the express server
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
     
@@ -44,12 +47,39 @@ app.post("/login", async (req, res) => {
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if(isPasswordValid) {
-            res.send("Login successfully.")
+            //create jwt token
+            const token = await jwt.sign({ _id: user._id }, "DEVTEAM@123");
+            console.log(token);
+
+            //Add the token to cookie and send the response back to user
+            res.cookie("token", token);
+            res.send("Login Successful!!");
         } else {
             throw new Error("Invalid credentails");
         }
     } catch(err) {
         res.status(400).send("Error: "+err.message);
+    }
+}); 
+
+app.get("/profile", async (req, res) => {
+    try {
+        const cookies = req.cookies;
+    
+        const { token } = cookies;
+        if(!token) {
+            throw new Error("Token not found");
+        }
+        //Validate my token
+        const decodedMessage = await jwt.verify(token, "DEVTEAM@123");
+        const { _id } = decodedMessage;
+        const user = await User.findById(_id);
+        if(!user) {
+            throw new Error("User not found");
+        }
+        res.send(user);
+    } catch(err) {
+        res.status(400).send("Some error: " + err.message);
     }
 });
 
