1a636c1e6eb64e595c64a5981c8c7670387867e2
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-05-17 12:59:50 +0530

    cors https error bypassed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const app = express(); //creating the express server
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
